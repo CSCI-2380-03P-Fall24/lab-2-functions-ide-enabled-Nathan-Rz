4d3f5c324288c7627b4fa60cc8b3fd9f2c132460
Nathan-Rz <nathan.rodriguez08@utrgv.edu>
2025-09-15 03:50:31 +0000

    complete lab

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -11,7 +11,15 @@ using namespace std; // making life easier so that we do not need to use std::ci
 
 
 int locateSmallest(int array[], int start, int end){
-	return -1; // fix this!
+	int smallest = 0;
+
+	for (int i = start; i < end - 1; i++){
+		if (array[i] <= array[i + 1] && array[i] <= array[smallest]){
+			smallest = i;
+		}
+	}
+
+	return smallest; // fix this!
 }
 
 //Write the function called replaceVariable in the given interval such that 
@@ -20,8 +28,14 @@ int locateSmallest(int array[], int start, int end){
 //     an int which is the starting index of search,
 //     an int which is the ending point of searchspace
 // function: Search in the array from given start to end index and replace the given target value with 1000
-void replaceVariable() {
-	return;
+void replaceVariable(int array[], int tar, int sta, int en) {
+
+	for (int i = sta; i < en; i++){
+		if (array[i] == tar){
+			array[i] = 1000;
+		}
+	}
+
 }
 
 
@@ -29,8 +43,14 @@ void replaceVariable() {
 //write a function called printArray to print out the elements of the given array
 //output: nothing
 //input: the array of integers and its size
-void printArray() {
-	return;
+void printArray(int array[]) {
+
+	int size = sizeof(array)/sizeof(array[0]);
+	for (int i = 0; i < size; i++ ){
+		cout << i;
+	}
+
+	cout << size;
 }
 
 
